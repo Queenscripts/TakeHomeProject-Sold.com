4161b98d668829e1ffe33b32986ca665dc36b9b7
Queen <queensform@Queens-MacBook-Air.local>
2021-02-27 18:42:54 -0800

    formatted form + form inputs, added error messages, removed helper logs

--- src/components/signup/SignUp.tsx
+++ src/components/signup/SignUp.tsx
@@ -1,10 +1,12 @@
 import { Button, FormControl, FormHelperText, Input, InputLabel } from '@material-ui/core';
-import { default as React } from 'react';
+import { AccountCircle, Fingerprint, Lock, MailOutline, Phone } from '@material-ui/icons';
 import { useRouter } from 'next/router';
+import { default as React } from 'react';
+import styles from './SignUp.module.css';
 
 export const SignUp = () => {
   const router = useRouter();
-  //   const [error, setError] = React.useState<string>();
+  const [error, setError] = React.useState<string>();
   const [formValues, setFormValues] = React.useState({});
 
   const handleChangeFormValues = (key: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -12,13 +14,11 @@ export const SignUp = () => {
       ...formValues,
       [key]: e.target.value,
     });
-    console.log(formValues);
   };
 
   const handleSignUp = async (e) => {
     e.preventDefault();
     let json;
-    console.log(formValues);
     try {
       const res = await fetch(`http://localhost:9001/users`, {
         method: 'POST',
@@ -30,68 +30,106 @@ export const SignUp = () => {
       json = await res.json();
       if (!res.ok) {
         throw new Error(JSON.stringify(json));
+      } else {
+        router.push('/login');
       }
     } catch (e) {
-      router.push('/dashboard');
-      console.log(json, '\n', e);
+      setError(
+        json.message === 'User already exists.' ? (
+          Object.keys(json.keyValue)[0] + ' is taken. Please enter a new value.'
+        ) : typeof json.message != 'string' ? (
+          <ul>
+            {json.message.map((i) => (
+              <li key={i}>{i}</li>
+            ))}
+          </ul>
+        ) : (
+          json.message
+        ),
+      );
+
+      //   console.log(json, '\n', e);
     }
   };
 
   return (
     <div>
-      <div>
+      <div className={styles.container}>
         <h1>Signup</h1>
-        <FormControl>
-          <InputLabel htmlFor='my-input'>First Name</InputLabel>
-          <Input
-            onChange={handleChangeFormValues('firstName')}
-            defaultValue={'firstname'}
-            id='name-input'
-            aria-describedby='my-helper-text'
-          />
-          <FormHelperText id='email-helper-text'>We'll never share your email.</FormHelperText>
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor='my-input'>Last Name</InputLabel>
-          <Input
-            onChange={handleChangeFormValues('lastName')}
-            defaultValue={'last'}
-            id='last-input'
-            aria-describedby='my-helper-text'
-          />
-          <FormHelperText id='last-helper-text'>We'll never share your email.</FormHelperText>
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor='my-input'>Email address</InputLabel>
-          <Input
-            onChange={handleChangeFormValues('email')}
-            defaultValue={'email'}
-            id='email-input'
-            aria-describedby='my-helper-text'
-          />
-          <FormHelperText id='email-helper-text'>We'll never share your email.</FormHelperText>
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor='my-input'>Password</InputLabel>
-          <Input
-            defaultValue={'password'}
-            onChange={handleChangeFormValues('password')}
-            id='my-input'
-            aria-describedby='my-helper-text'
-          />
-          <FormHelperText id='my-helper-text'>Don't share your password.</FormHelperText>
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor='my-input'>Phone Number</InputLabel>
-          <Input
-            onChange={handleChangeFormValues('phoneNumber')}
-            defaultValue={'phone'}
-            id='phone-input'
-            aria-describedby='my-helper-text'
-          />
-          <FormHelperText id='phone-helper-text'>We'll never share your email.</FormHelperText>
-        </FormControl>
-
+        <form className={styles.form}>
+          <FormControl>
+            <InputLabel htmlFor='my-input'>
+              <AccountCircle /> First Name
+            </InputLabel>
+            <Input
+              onChange={handleChangeFormValues('firstName')}
+              placeholder={'Xan'}
+              id='name-input'
+              aria-describedby='my-helper-text'
+            />
+            <FormHelperText id='email-helper-text'>
+              Please enter your full first name.
+            </FormHelperText>
+          </FormControl>
+          <FormControl>
+            <InputLabel htmlFor='my-input'>
+              <Fingerprint /> Last Name
+            </InputLabel>
+            <Input
+              onChange={handleChangeFormValues('lastName')}
+              placeholder={'Michel'}
+              id='last-input'
+              aria-describedby='my-helper-text'
+            />
+            <FormHelperText id='last-helper-text'>
+              Please enter your full last name.
+            </FormHelperText>
+          </FormControl>
+          <FormControl>
+            <InputLabel htmlFor='my-input'>
+              <MailOutline />
+              Email address
+            </InputLabel>
+            <Input
+              onChange={handleChangeFormValues('email')}
+              placeholder={'xan@michel.com'}
+              id='email-input'
+              aria-describedby='my-helper-text'
+            />
+            <FormHelperText id='email-helper-text'>
+              We'll never share your email.
+            </FormHelperText>
+          </FormControl>
+          <FormControl>
+            <InputLabel htmlFor='my-input'>
+              <Lock />
+              Password
+            </InputLabel>
+            <Input
+              placeholder={'T0pSecret!'}
+              onChange={handleChangeFormValues('password')}
+              id='my-input'
+              type='password'
+              aria-describedby='my-helper-text'
+            />
+            <FormHelperText id='my-helper-text'>Don't share your password.</FormHelperText>
+          </FormControl>
+          <FormControl>
+            <InputLabel htmlFor='my-input'>
+              {' '}
+              <Phone /> Phone Number
+            </InputLabel>
+            <Input
+              onChange={handleChangeFormValues('phoneNumber')}
+              placeholder={'+15555555'}
+              id='phone-input'
+              aria-describedby='my-helper-text'
+            />
+            <FormHelperText id='phone-helper-text'>
+              Follow the phone number pattern.
+            </FormHelperText>
+          </FormControl>
+        </form>
         <Button onClick={handleSignUp} variant='contained'>
           Sign Up
         </Button>
@@ -101,7 +139,7 @@ export const SignUp = () => {
             </Button>
           </div> */}
         {/* Handle errors + state to popup in DOM */}
-        {/* <div>{error && <p style={{ color: 'red' }}>{error}</p>}</div> */}
+        <div style={{ color: 'red' }}>{error}</div>
       </div>
     </div>
   );
